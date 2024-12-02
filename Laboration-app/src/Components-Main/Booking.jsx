ee2338ce9afae8f5ac77a1852f68188af3a77592
t.hallberg84 <t.hallberg84@gmail.com>
2024-12-02 13:46:12 +0100

    About.jsx

---
+++ Laboration-app/src/Components-Main/Booking.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Typography, Button, Paper,Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Booking = () => {
+  return (
+    <Container className="booking-section">
+      <Paper className="section-paper">
+        <Typography variant="h2">
+          Boka ditt besök</Typography>
+
+        <Typography variant="body1" >
+          Upplev skönheten hos de isländska hästarna! Klicka på knappen nedan
+          för att boka ditt besök. Följ med oss på en oförglömlig upplevelse med
+          de isländska hästarna. Oavsett om du är intresserad av ridning eller
+          bara vill lära dig mer om dessa otroliga djur, erbjuder vi olika
+          bokningsalternativ som passar dina behov.
+          <Box className="divider"></Box>
+        </Typography>
+
+        <Typography variant="body2">
+          För att göra en bokning eller få mer information, klicka på knappen
+          nedan för att gå vidare till vår bokningssida.
+        </Typography>
+
+        <Button 
+          variant="contained"
+          color="primary"
+          size="large"
+          component={Link}
+          to="/booking"
+        >
+          Boka nu
+        </Button>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Booking;
--- Laboration-app/src/Components/Main.jsx
+++ Laboration-app/src/Components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../Components-Main/About'
+import Booking from '../Components-Main/Booking'
 
 const Main = () => {
   return (
     <div>
         <About/>
+        <Booking/>
     </div>
   )
 }
