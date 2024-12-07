89b2af5f17d175062bca4bf115990a631fc54b9c
t.hallberg84 <t.hallberg84@gmail.com>
2024-12-07 17:05:37 +0100

    css så att bookingFormPage sträcker sig över hela sidan. nav top, åh footer bottom krågligt värre

--- Laboration-app/src/App.jsx
+++ Laboration-app/src/App.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import "./Styles/Styles.css"
-import HomePage from "./Pages/HomePage"
+import "./Styles/Styles.css";
+import HomePage from "./Pages/HomePage";
 import BookingPage from "./Pages/BookingPage";
 import Navbar from "./Components/navbar";
 import Footer from "./Components/Footer";
 import BookingFormPage from "./Pages/BookingFormPage";
-import { Box } from "@mui/material";
-
-
-
-
 
 function App() {
   return (
-    <BrowserRouter> 
-      <Box id="root">
-        <Navbar />
-        <Box className="content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/booking" element={<BookingPage />} />
-            <Route path="/Booking-form/:id" element={<BookingFormPage />} />
-          </Routes>
-        </Box>
-        <Footer />
-      </Box>
-    </BrowserRouter> 
+    <div className="App">
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/booking" element={<BookingPage />} />
+          <Route path="/Booking-form/:id" element={<BookingFormPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  </div>
   );
 }
 
